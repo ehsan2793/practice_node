15ea49e8d6766d9522af6c98080fd3ed68cc0680
ehsan2793 <ehsan2793@gmail.com>
2024-04-13 16:49:14 -0700

    added more route

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const express = require('express');
 
 const app = express();
+app.use(express.json());
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
@@ -10,10 +11,37 @@ const tours = JSON.parse(
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
     status: 200,
+    results: tours.length,
     data: {
       tours,
     },
   });
+});
+
+app.get('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id;
+  const tour = tours.find((tour) => (tour.id = id));
+  res.send(tour);
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 const port = 3000;
