d081114f46db10d01dcb5c95a697f88934316281
ehsan2793 <ehsan2793@gmail.com>
2024-04-13 09:52:13 -0700

    added first api

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Hello' });
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 200,
+    data: {
+      tours,
+    },
+  });
 });
 
 const port = 3000;
