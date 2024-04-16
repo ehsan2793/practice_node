33c7925ea730da7189f82eaec4b72cc738824704
ehsan2793 <ehsan2793@gmail.com>
2024-04-15 19:54:15 -0700

    added middleware

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -3,6 +3,17 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 );
 
+exports.checkID = (req, res, next, val) => {
+  if (val * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'invalid Id',
+    });
+  }
+
+  next();
+};
+
 exports.getAllTours = (req, res) => {
   res.status(200).json({
     status: 200,
@@ -52,8 +63,6 @@ exports.createTour = (req, res) => {
 };
 
 exports.updateTour = (req, res) => {
-  const id = +req.params.id;
-  const tour = tours.find((tour) => tour.id === id);
   res.status(200).json({
     status: 'success',
     data: {
