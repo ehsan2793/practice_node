49a25da8223661271ded8666f1dae5d47d02df47
ehsan2793 <ehsan2793@gmail.com>
2024-04-17 22:46:45 -0700

    added env

--- app.js
+++ app.js
@@ -4,10 +4,14 @@ const morgan = require('morgan');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const app = express();
-
 // Middlewared
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+console.log(process.env);
+
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
 const app = require('./app');
 
-const port = 3000;
-app.listen(port, () => {
-  console.log(`App running on port ${port}`);
+PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}`);
 });
