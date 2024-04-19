71aed82449a0c203e74ae447f0f229362fa2cf73
ehsan2793 <ehsan2793@gmail.com>
2024-04-18 22:33:22 -0700

    added

--- server.js
+++ server.js
@@ -6,5 +6,6 @@ const app = require('./app');
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
+  /* eslint-disable-next-line */
   console.log(`App running on port ${PORT}`);
 });
