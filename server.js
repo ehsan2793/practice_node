8093fa893a085675f3aee7bc76befd5438c7e13e
ehsan2793 <ehsan2793@gmail.com>
2024-04-21 22:55:08 -0700

    added mongo

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose
+  .connect(DB)
+  .then(() => {
+    /* eslint-disable-next-line */
+    console.log('DB conntected');
+  })
+  .catch((err) => {
+    /* eslint-disable-next-line */
+    console.error('DB connection error:', err);
+  });
+
 const app = require('./app');
 
 const PORT = process.env.PORT || 3001;
--- serverStatus.js
+++ serverStatus.js
@@ -1,40 +1,39 @@
 // Define an enum for server status and messages
-const ServerStatus = {
-  SUCCESS: { code: 200, message: 'Success' },
-  CREATED: { code: 201, message: 'Resource Created' },
-  BAD_REQUEST: { code: 400, message: 'Bad Request' },
-  UNAUTHORIZED: { code: 401, message: 'Unauthorized' },
-  FORBIDDEN: { code: 403, message: 'Forbidden' },
-  NOT_FOUND: { code: 404, message: 'Not Found' },
-  INTERNAL_SERVER_ERROR: { code: 500, message: 'Internal Server Error' },
-  SERVICE_UNAVAILABLE: { code: 503, message: 'Service Unavailable' },
-};
+// const ServerStatus = {
+//   SUCCESS: { code: 200, message: 'Success' },
+//   CREATED: { code: 201, message: 'Resource Created' },
+//   BAD_REQUEST: { code: 400, message: 'Bad Request' },
+//   UNAUTHORIZED: { code: 401, message: 'Unauthorized' },
+//   FORBIDDEN: { code: 403, message: 'Forbidden' },
+//   NOT_FOUND: { code: 404, message: 'Not Found' },
+//   INTERNAL_SERVER_ERROR: { code: 500, message: 'Internal Server Error' },
+//   SERVICE_UNAVAILABLE: { code: 503, message: 'Service Unavailable' },
+// };
 
 // Export the enum for use in other modules
 
-
 // Utility functions to work with the enum
-const getStatusCode = (status) => {
-  return status.code;
-};
+// const getStatusCode = (status) => {
+//   return status.code;
+// };
 
-const getStatusMessage = (status) => {
-  return status.message;
-};
+// const getStatusMessage = (status) => {
+//   return status.message;
+// };
 
-const getStatusFromCode = (code) => {
-  for (const key in ServerStatus) {
-    if (ServerStatus[key].code === code) {
-      return ServerStatus[key];
-    }
-  }
-  return null;
-};
+// const getStatusFromCode = (code) => {
+//   for (const key in ServerStatus) {
+//     if (ServerStatus[key].code === code) {
+//       return ServerStatus[key];
+//     }
+//   }
+//   return null;
+// };
 
 // Export utility functions
-module.exports = {
-  ServerStatus,
-  getStatusCode,
-  getStatusMessage,
-  getStatusFromCode,
-};
+// module.exports = {
+//   ServerStatus,
+//   getStatusCode,
+//   getStatusMessage,
+//   getStatusFromCode,
+// };
