a5ca7100db725d5d1baeb71fb9da57d6cc258b9c
ehsan2793 <ehsan2793@gmail.com>
2024-04-23 19:17:55 -0700

    added create

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,76 +1,48 @@
-const fs = require('fs');
-
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`),
-);
-
-exports.checkID = (req, res, next, val) => {
-  if (val * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'invalid Id',
-    });
-  }
-
-  next();
-};
-
-exports.checkBody = (req, res, next) => {
-  if (!req.body.name || !req.body.price) {
-    return res.status(400).json({
-      status: 'fail',
-      message: 'Missing name or price',
-    });
-  }
-
-  next();
-};
+const Tour = require('../models/tourModel');
 
 exports.getAllTours = (req, res) => {
   res.status(200).json({
-    status: 200,
-    results: tours.length,
-    data: {
-      tours,
-    },
+    status: 'sucess',
+    // results: tours.length,
+    // data: {
+    //   tours,
+    // },
   });
 };
 
 exports.getTour = (req, res) => {
-  const id = +req.params.id;
-  const tour = tours.find((item) => item.id === id);
-  if (!tour) {
-    return res.status(404).json({
-      status: 'Failed',
-      message: 'Invalid ID',
-    });
-  }
-  res.status(200).json({
-    status: 'sucess',
-    data: {
-      tour,
-    },
-  });
+  // const id = +req.params.id;
+  // const tour = tours.find((item) => item.id === id);
+  // if (!tour) {
+  //   return res.status(404).json({
+  //     status: 'Failed',
+  //     message: 'Invalid ID',
+  //   });
+  // }
+  // res.status(200).json({
+  //   status: 'sucess',
+  //   data: {
+  //     tour,
+  //   },
+  // });
 };
 
-exports.createTour = (req, res) => {
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, ...req.body);
+exports.createTour = async (req, res, next) => {
+  try {
+    const newTour = await Tour.create(req.body);
 
-  tours.push(newTour);
-
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    () => {
-      res.status(201).json({
-        status: 'success',
-        data: {
-          tour: newTour,
-        },
-      });
-    },
-  );
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      message: error,
+    });
+  }
 };
 
 exports.updateTour = (req, res) => {
